75c9f330c1972a1c6e9bf2c3cff22fe36e8677b9
Paula Valentina Lozano Castañeda <paula.lozano-c@mail.escuelaing.edu.co>
2026-02-28 13:29:04 -0500

    Feat: parte 2 parcial listo.

---
+++ DOSW-ParcialT1/src/test/java/edu/dosw/parcial/DOSW_ParcialT1/Disponibilidad.java
@@ -0,0 +1,14 @@
+package edu.dosw.parcial.DOSW_ParcialT1;
+import java.util.ArrayList();
+
+public class Disponibilidad() {
+    private disponibles = List();
+
+    public boolean disponible(fecha, cupo, nombre) {
+
+    } 
+
+    public void registrar(evento) {
+
+    }
+}
---
+++ DOSW-ParcialT1/src/test/java/edu/dosw/parcial/DOSW_ParcialT1/Taller.java
@@ -0,0 +1,6 @@
+package edu.dosw.parcial.DOSW_ParcialT1;
+
+public class Taller implements TipoEvento() {
+    crearConferencia(rrhh: RRHHAdapter, dispo: Disponibilidad);
+    validar(e : Evento)
+}
